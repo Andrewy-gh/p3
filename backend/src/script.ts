297204aa806c443d020494a347cd55fdcc42a42b
Andy-git985 <andy.yu617@gmail.com>
2025-09-08 08:55:35 -0400

    experimental output example

--- backend/src/script.ts
+++ backend/src/script.ts
@@ -1,5 +1,6 @@
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
-import { generateText, streamText } from 'ai';
+import { generateText, streamText, Output } from 'ai';
+import { z } from 'zod';
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.GOOGLE_GENERATIVE_AI_API_KEY,
@@ -30,11 +31,35 @@ console.dir(model, { depth: null });
 // }
 
 // 01.5 - ui message streams
-const stream = streamText({
+// const stream = streamText({
+//   model,
+//   prompt: 'Give me a sonnet about a cat called Steven.',
+// });
+
+// for await (const chunk of stream.toUIMessageStream()) {
+//   console.log(chunk);
+// }
+
+// experimental output example
+// https://ai-sdk.dev/docs/ai-sdk-core/generating-structured-data#structured-output-with-generatetext
+const { experimental_output } = await generateText({
   model,
-  prompt: 'Give me a sonnet about a cat called Steven.',
+  experimental_output: Output.object({
+    schema: z.object({
+      name: z.string(),
+      age: z.number().nullable().describe('Age of the person.'),
+      contact: z.object({
+        type: z.literal('email'),
+        value: z.string(),
+      }),
+      occupation: z.object({
+        type: z.literal('employed'),
+        company: z.string(),
+        position: z.string(),
+      }),
+    }),
+  }),
+  prompt: 'Generate an example person for testing.',
 });
 
-for await (const chunk of stream.toUIMessageStream()) {
-  console.log(chunk);
-}
+console.log(experimental_output);
